4a3f5254ca6236de1aa01832c9eeeea2bba8d23a
Alexander <alexander.enge@hu-berlin.de>
2024-11-19 16:22:41 +0100

    Remove unneeded scripts from BIDS conversion

--- scripts/clean_tmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fail whenever something is fishy; use -x to get verbose logfiles
-set -e -u -x
-
-# Clean up the temporary directory
-tmp_dir="/ptmp/$USER/tmp/"
-chmod -R +wrx "$tmp_dir"
-rm -rf "$tmp_dir"
